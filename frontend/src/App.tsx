f5ccf9f9b923f3e64a68213cbb70a36b6e2efbb7
Zakary Nichols <zakarynichols@gmail.com>
2025-09-20 00:41:35 -0400

    remove vite default template

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,10 +1,7 @@
 import { useEffect, useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [response, setResponse] = useState("")
 
   const backendUrl = import.meta.env.VITE_BACKEND_URL;
 
@@ -12,37 +9,17 @@ function App() {
 
   useEffect(() => {
     fetch(`${backendUrl}`)
-      .then((res) => res.text())
-      .then((text) => console.log("✅ Backend says:", text))
-      .catch((err) => console.error("❌ Backend error:", err));
+      .then((res) => res.json())
+      .then((json) => {
+        setResponse(JSON.stringify(json))
+      })
   }, []);
 
 
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => {
-          setCount((count) => count + 1)
-        }}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+    <div>Workout log app
+      <p>Server response: {response}</p>
+    </div>
   )
 }
 
